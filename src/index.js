5543bc29b30159966cf246191bc3e8a768a92e73
Reza M Fauzi <rezayuyara@gmail.com>
2024-04-09 21:24:11 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from "./App.jsx";
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const entryPoint = document.getElementById("root");
+// ReactDOM.createRoot(entryPoint).render(<App />); 
+ReactDOM.createRoot(entryPoint).render(React.createElement(App)); 
